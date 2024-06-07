d982d017ce48431fad0e877f2f863e0fe678d8c1
manifestations <wealthlovesuccesshealth@manifesionssAir.fritz.box>
2024-06-07 13:15:45 +0200

    modify settings.py

--- sunny/settings.py
+++ sunny/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['celta.herokuapp.com']
+
+ALLOWED_HOSTS = ['celta.vscode.com']
 
 
 # Application definition
